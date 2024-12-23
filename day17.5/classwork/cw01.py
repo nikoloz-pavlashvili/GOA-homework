a54725d6c53f36ac7970f766b262af094a2a9ea4
nikoloz-pavlashvili <1508nikolozi@gmail.com>
2024-12-23 23:03:18 +0400

    second commit

---
+++ day17.5/classwork/cw01.py
@@ -0,0 +1,13 @@
+#2)გააკეთეთ 5 მაგალითი ლოგიკურ ოპერატორებზე , ხოლო 5 მაგალითი შედარების ოპერატორებზე
+
+print(True and False)
+print(True or False)
+print(False or True)
+print(not True)
+print(not False)
+
+print(55 > 67) #false
+print(77<888)#true
+print(555>=252525) #false
+print(777<=7686) #true
+print(1111==1111) #true
---
+++ day17.5/homework/hw01.py
@@ -0,0 +1,10 @@
+#1)დაწერეთ პროგრამა, რომელიც ითხოვს მომხმარებლის სახელს და ამის შემდეგ ბეჭდავს: "გამარჯობა, სახელი!"
+#2)მომხმარებელს შემოატანინეთ მასზე ინფორმაცია და ეს ყველაფერი ერთ print-ში დაბეჭდეთ
+
+name=input("enter your name")
+
+print("hallo " + name)
+
+name=input("enteryour name")
+surname=("enter your surname")
+print(name,surname)
