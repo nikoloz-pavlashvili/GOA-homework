6f9068ccc3789685801292fe5b9fe37cb91c272e
nikoloz-pavlashvili <1508nikolozi@gmail.com>
2025-01-22 22:52:00 +0400

    second commit

---
+++ day22/classwork/cw01.py
@@ -0,0 +1,16 @@
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+print("nikoloz pavliashvili")
+
+for i in range(10)
+    print("nikoloz pavliashvili")
+
+
+#ვირჯინი გზა ძალიან დიდ დროს და ადგილს მოითხოვს როცა ჩადური გზით უფროპროესიონალურიც გმოჩნდები და ძალიან მალე მორჩები
---
+++ day22/classwork/cw02.py
@@ -0,0 +1,12 @@
+#3) for ციკლის საშვალებით გამოიტანეთ რიცხვები 5-ის ჩათვილითი
+
+for i in range(6):
+    print(i)
+for i in range(5):
+    print(i)
+for i in range(10 , 50):
+    print(i)
+for i in range(5,15,2):
+    print(i)
+
+
